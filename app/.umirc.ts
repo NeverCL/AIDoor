077b21933db6d571525c1c33fdb6cd2335759f15
Your Name <you@example.com>
2025-04-07 09:54:06 +0800

    增加 SafeArea 能力

--- app/.umirc.ts
+++ app/.umirc.ts
@@ -51,5 +51,9 @@ export default defineConfig({
       name: 'viewport',
       content: 'width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no',
     },
+    {
+      name: 'viewport',
+      content: 'width=device-width'
+    }
   ],
 });
--- app/src/layouts/index.tsx
+++ app/src/layouts/index.tsx
@@ -1,12 +1,29 @@
 import FloatBtn from "@/components/FloatBtn"
-import NavHeader from "@/components/NavHeader"
 import { Outlet } from "@umijs/max"
+import { SafeArea } from "antd-mobile"
+import { useEffect, useState } from "react"
 
 export default () => {
+    const [windowHeight, setWindowHeight] = useState(window.innerHeight);
+
+    useEffect(() => {
+        function handleResize() {
+            setWindowHeight(window.innerHeight);
+        }
+
+        window.addEventListener('resize', handleResize);
+        return () => window.removeEventListener('resize', handleResize);
+    }, []);
+
     return (
         <>
-            <div className="flex flex-col px-4 h-[100vh] overflow-y-auto overflow-x-hidden text-secondary text-xs">
-                <Outlet />
+
+            <div className="flex flex-col px-4 overflow-y-auto overflow-x-hidden text-secondary text-xs" style={{ height: windowHeight }}>
+                <SafeArea position={"top"} />
+                <div className="flex flex-col flex-1 overflow-y-auto overflow-x-hidden">
+                    <Outlet />
+                </div>
+                <SafeArea position={"bottom"} />
             </div>
 
             <FloatBtn />
